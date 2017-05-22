bbd9b071bde11dc99ed16044e04ac20ab7f48fa9
nishanthnamana <nishanthnamana@nishanths-mbp.attlocal.net>
2017-05-22 01:37:45 -0500

    Modified the code for responsiveness for devices with width less than 480 px.

--- js/custom.js
+++ js/custom.js
@@ -31,7 +31,21 @@ $(function () {
         smartSpeed: 800,
         loop: true,
         autoplayHoverPause: true,
-        autoplaySpeed: 900
+        autoplaySpeed: 900,
+        responsive : {
+    // breakpoint from 0 up
+    0 : {
+        items : 1
+    },
+    // breakpoint from 480 up
+    480 : {
+        items : 2
+    },
+    // breakpoint from 768 up
+    768 : {
+        items : 3
+    }
+        }
     });
 });
 
